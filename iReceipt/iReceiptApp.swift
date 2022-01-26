8400602b9a521ae5222a7d6af2832b57b5dc8a40
Code Palace <codepalace@fastmail.se>
2022-01-26 12:49:05 +0100

    Initial Commit

---
+++ iReceipt/iReceiptApp.swift
@@ -0,0 +1,17 @@
+//
+//  iReceiptApp.swift
+//  iReceipt
+//
+//  Created by Federico on 26/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct iReceiptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
